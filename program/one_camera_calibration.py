0f2707cbcac63b69dcfa69f687c62faf5c5e0e9e
Jana Bátoryová <jbatoryova@trask.cz>
2017-12-09 12:05:51 +0100

    Starting to write stereo calibration

--- program/one_camera_calibration.py
+++ program/one_camera_calibration.py
@@ -3,13 +3,15 @@ import cv2
 import glob
 
 
-class ChessboardFinder(object):
+class MonoCameraCalibration(object):
 
     def __init__(self, chessboard_image):
+        self.name = chessboard_image
         self.images = glob.glob(chessboard_image)
         self.objpoints = []
         self.imgpoints = []
         self.size = (7,9)
+        self.sucessful = []
 
     def find_chessboad(self):
         # termination criteria
@@ -22,7 +24,7 @@ class ChessboardFinder(object):
         objp = np.zeros((size[0] * size[1], 3), np.float32)
         objp[:, :2] = np.mgrid[0:size[0], 0:size[1]].T.reshape(-1, 2)
 
-        for fname in self.images:
+        for i, fname in enumerate(self.images):
             img = cv2.imread(fname)
             self.h, self.w = img.shape[:2]
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -31,9 +33,9 @@ class ChessboardFinder(object):
             ret, corners = cv2.findChessboardCorners(gray, self.size, None)
 
             # If found, add object points, image points (after refining them)
-            if ret == True:
+            if ret:
+                self.sucessful.append(i)
                 self.objpoints.append(objp)
-                # print(objp)
                 corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
                 self.imgpoints.append(corners2)
 
@@ -61,18 +63,48 @@ class ChessboardFinder(object):
         return mean_error / len(self.objpoints)
 
 
+class StereoCameraCalibration(object):
+    def __init__(self, mono_calibrations: [MonoCameraCalibration]) -> object:
+        self.mono_calibrations = mono_calibrations
+
+    def combine(self):
+        list(sorted(set.intersection(*[set(calibration.successful) for calibration in self.mono_calibrations])))
+
+
+
+path_to_images = 'images/two_camera_calibration/'
 
 finders = [
-    ChessboardFinder('images/single_camera_calibration/left/*.jpg'),
-    #ChessboardFinder('images/single_camera_calibration/right/*.jpg'),
+    MonoCameraCalibration(path_to_images + 'left/*.jpg'),
+    MonoCameraCalibration(path_to_images + 'right/*.jpg'),
 ]
 
 for finder in finders:
+    print(finder.name)
     finder.find_chessboad()
+    finder.calibrate()
+    print(finder.sucessful)
+    print(finder.compute_error())
 
-finders[0].calibrate()
-print(finders[0].compute_error())
-undistort = finders[0].undistort('images/single_camera_calibration/left/WIN_20171208_23_28_07_Pro.jpg')
-cv2.imwrite('images/single_camera_calibration/calibresult.png', undistort)
+
+
+#c1 = finders[0]
+#c2 = finders[1]
+#objpoints = []
+#R = []
+#T = []
+#E = []
+#F = []
+#cv2.stereoCalibrate(objpoints, c1.imgpoints, c2.imgpoints, c1.mtx, c1.dist, c2.mtx, c2.dist, c1.size, R, T, E, F,
+#                    cv2.cvTermCriteria(cv2.CV_TERMCRIT_ITER+cv2.CV_TERMCRIT_EPS, 100, 1e-5),
+#                    cv2.CV_CALIB_SAME_FOCAL_LENGTH | cv2.CV_CALIB_ZERO_TANGENT_DIST)
+
+
+
+
+#finders[0].calibrate()
+#print(finders[0].compute_error())
+#undistort = finders[0].undistort('images/single_camera_calibration/left/WIN_20171208_23_28_07_Pro.jpg')
+#cv2.imwrite('images/single_camera_calibration/calibresult.png', undistort)
 
 cv2.destroyAllWindows()
