2bb7519abdff7c6215debbd2a05725f5a42b6a47
Jana Bátoryová <jbatoryova@trask.cz>
2017-12-15 14:00:50 +0100

    Finished wihh processes

--- program/camera_input.py
+++ program/camera_input.py
@@ -23,33 +23,37 @@ class VideoProvider(object):
     def stop_capturing(self):
         self.work = False
 
-    def capturing(self):
-        while self.work:
-            ret, frame = self.capture.read()
-            if ret:
-                print("Adding image")
-                images.put(frame)
+    def capturing(self, images):
+        ret, frame = self.capture.read()
+        if ret:
+            print("Adding image")
+            images.put(frame, False)
+            #print(images.qsize())
+
 
 class CameraWrapper(object):
-    def __init__(self, camera_index):
+    def __init__(self, camera_index, images):
         self.camera_index = camera_index
-        process = Process(target=self.run_camera)
+        process = Process(target=self.run_camera, args=(images,))
         process.daemon = True
         process.start()
 
-    def run_camera(self):
+
+    def run_camera(self, images):
         provider = VideoProvider(self.camera_index)
         provider.setup_camera(640,480)
         self.running = True
-        provider.start_capturing()
+        while self.running:
+            provider.capturing(images)
+        print("Am I running", self.running)
 
-cameras = [1]
-images = SimpleQueue()
+cameras = [0]
+images = Queue()
 if __name__ == '__main__':
     # spominane, ze len problem windowsov
     ## https://stackoverflow.com/questions/18204782/runtimeerror-on-windows-trying-python-multiprocessing
 
-    wrap = CameraWrapper(cameras[0])
+    wrap = CameraWrapper(cameras[0], images)
 
     time.sleep(2)
 
@@ -58,10 +62,11 @@ if __name__ == '__main__':
     print(images.empty())
 
     time.sleep(1)
+    print("setting false")
     CameraWrapper.running = False
 
     time.sleep(5)
-    print(images.qsize())
+    print(images.empty())
 
     print("Stopping")
 
