b0ae12764e2dd45c0ebd3d5fceb22349ab1d85c6
Jana Bátoryová <janulik11@gmail.com>
2018-04-17 00:18:43 +0200

    Fixing bug in pattern matching

--- program/project/TrackerPatternMatching.py
+++ program/project/TrackerPatternMatching.py
@@ -5,12 +5,12 @@ class TrackerPatternMatching(object):
 
     def init(self, image, bbox):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        self.pattern = image[bbox[0]:bbox[2] + bbox[0], bbox[1]:bbox[3] + bbox[1]]
+        self.pattern = image[bbox[1]:bbox[3] + bbox[1], bbox[0]:bbox[2] + bbox[0]]
         self.height, self.width = self.pattern.shape
         return True
 
     def update(self, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        res = cv2.matchTemplate(image, self.pattern, cv2.TM_CCOEFF)
+        res = cv2.matchTemplate(image, self.pattern, cv2.TM_SQDIFF)
         _, maxVal, _, maxLoc = cv2.minMaxLoc(res)
         return (True, (maxLoc[0], maxLoc[1], self.width, self.height))
